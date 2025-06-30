996807c490ebf012200dba9a9e554fcc6347bd44
Sergey Drozdov – Dev <r2wall@yandex.com>
2025-06-30 15:26:37 +0300

    Replace PayOK with YooKassa

--- main.py
+++ main.py
@@ -44,11 +44,10 @@ async def main() -> None:
     sessionmaker = await create_sessionmaker(config.db)
     payment = (
         payments.BasePayment() if not config.payments.enabled
-        else payments.PayOK(
-            config.payments.api_id,
-            config.payments.api_key,
-            config.payments.project_id,
-            config.payments.project_secret,
+        else payments.YooKassa(
+            config.payments.shop_id,
+            config.payments.secret_key,
+            config.payments.return_url,
         )
     )
 
